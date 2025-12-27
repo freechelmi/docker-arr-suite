9bf3238185500f1fc6d27cec3ab8ff84be38ccc4
freechelmi <mm@ekimia.fr>
2025-12-27 21:05:18 +0100

    source local variables

--- media.sh
+++ media.sh
@@ -1,4 +1,29 @@
-DOMAIN=domain-registered-on-clouflare
-EMAIL_ADDRESS=email-for-letsencrypt
-NFS_SERVER=nfs_ip
-NFS_VOLUME=nfs_vol_path
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 {up|down|upgrade|init}"
+    exit 1
+fi
+source env/local.env
+
+case "$1" in
+    up)
+        sudo docker compose up -d 
+        ;;
+    down)
+        sudo docker compose down -v
+        ;;
+    upgrade)
+        sudo docker compose pull
+        sudo docker compose down -v
+        sudo docker compose up -d
+        ;;
+    init)
+        sudo docker compose pull
+        ;;
+    *)
+        echo "Invalid parameter: $1"
+        echo "Usage: $0 {up|down|upgrade|init}"
+        exit 1
+        ;;
+esac
